a82ddb41eb5da556bf8380de5a4f01f4499ad2c2
HuangYong <newfriday@126.com>
2019-11-30 13:34:52 +0800

    eventfd: add EFD_SEMAPHORE test, add demo for study of process memory
    Signed-off-by: HuangYong <newfriday@126.com>

--- eventfd/main.c
+++ eventfd/main.c
@@ -10,7 +10,8 @@ void *threadFunc()
 {
     uint64_t buffer;
     int rc;
-    while(1){
+    int i = 0;
+    while(i++ < 3){
         rc = read(efd, &buffer, sizeof(buffer));
 
         if(rc == 8){
@@ -23,21 +24,53 @@ void *threadFunc()
 
 int main()
 {
-    pthread_t tid;
+    pthread_t tid1, tid2;
     int rc;
     uint64_t buf = 3;
+    int flags = 0;
+    int initval = 5;
+    int i = 0;
+    void *status;
 
-    efd = eventfd(1,0);     // blocking阻塞等待
+    efd = eventfd(initval, flags);     // blocking阻塞等待
     if(efd == -1){
         perror("eventfd");
     }
 
     //create thread
-    if(pthread_create(&tid, NULL, threadFunc, NULL) < 0){
+    if(pthread_create(&tid1, NULL, threadFunc, NULL) < 0){
         perror("pthread_create");
     }
 
-    while(1){
+    while(i++ < 3){
+        buf += i;
+        rc = write(efd, &buf, sizeof(buf));
+
+        if(rc != 8){
+            perror("write");
+        }
+        sleep(2);
+    }//end while
+    pthread_join(tid1, &status);
+    close(efd);
+
+    buf = 3;
+    flags = EFD_SEMAPHORE;
+    initval = 5;
+    i = 0;
+
+    efd = eventfd(initval, flags);     // blocking阻塞等待, 信号量方式使用eventfd
+    if(efd == -1){
+        perror("eventfd");
+    }
+
+    //create thread
+    if(pthread_create(&tid2, NULL, threadFunc, NULL) < 0){
+        perror("pthread_create");
+    }
+
+    while(i++ < 3){
+        buf += i;
         rc = write(efd, &buf, sizeof(buf));
 
         if(rc != 8){
@@ -46,6 +79,7 @@ int main()
         sleep(2);
     }//end while
     close(efd);
+
     return 0;
 }
 
---
+++ hello/hello_another.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int adder(int a, int b, int c, int d,
+          int e, int f, int g, int h);
+
+int adder(int a, int b, int c, int d,
+          int e, int f, int g, int h)
+{
+    return a+b+c+d+e+f+g+h;
+}
+
+void main(void)
+{
+    int sum;
+    char *s = malloc(10);
+    memcpy(s, "xxxxxxxxx", 9);
+    s[9] = '\0';
+    sum = adder(1,2,3,4,5,6,7,8);
+    printf("s=%s,sum=%d\n",s,sum);
+    printf("hello world\n");
+    while(1)
+    {}
+}
+
+
