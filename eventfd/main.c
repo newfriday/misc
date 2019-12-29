6f517cf35906562a44c0070e542023569a07887e
HuangYong <newfriday@126.com>
2019-12-29 10:47:53 +0800

    eventfd: add 3 different way to test eventfd
    1) NON-SEMAPHORE BLOCK
    2) SEMAPHORE
    3) NONBLOCK
    
    Signed-off-by: HuangYong <newfriday@126.com>

--- eventfd/main.c
+++ eventfd/main.c
@@ -11,75 +11,94 @@ void *threadFunc()
     uint64_t buffer;
     int rc;
     int i = 0;
-    while(i++ < 3){
+    while(i++ < 2){
+        /* 如果计数器为0，read失败，要么阻塞，要么返回EAGAIN */
         rc = read(efd, &buffer, sizeof(buffer));
 
-        if(rc == 8){
-            printf("notify success\n");
+        if (rc == 8) {
+            printf("notify success, eventfd counter = %lu\n", buffer);
+        } else {
+            perror("read");
         }
+    }
+}
 
-        printf("rc = %llu, buffer = %lu\n",(unsigned long long)rc, buffer);
-    }//end while
+static void
+open_eventfd(unsigned int initval, int flags)
+{
+    efd = eventfd(initval, flags);
+    if (efd == -1) {
+        perror("eventfd");
+    }
+}
+
+static void
+close_eventfd(int fd)
+{
+    close(fd);
+}
+
+static void test(int counter)
+{
+    int rc;
+    pthread_t tid;
+    void *status;
+    int i = 0;
+    uint64_t buf = 2;
+
+    /* create thread */
+    if(pthread_create(&tid, NULL, threadFunc, NULL) < 0){
+        perror("pthread_create");
+    }
+
+    while(i++ < counter){
+        rc = write(efd, &buf, sizeof(buf));
+        printf("signal to subscriber success, value = %lu\n", buf);
+
+        if(rc != 8){
+            perror("write");
+        }
+        sleep(2);
+    }
+
+    pthread_join(tid, &status);
 }
 
 int main()
 {
-    pthread_t tid1, tid2;
-    int rc;
-    uint64_t buf = 3;
-    int flags = 0;
-    int initval = 5;
-    int i = 0;
-    void *status;
+    unsigned int initval;
 
-    efd = eventfd(initval, flags);     // blocking阻塞等待
-    if(efd == -1){
-        perror("eventfd");
-    }
+    printf("NON-SEMAPHORE BLOCK way\n");
+    /* 初始值为54 flags为0，默认blocking方式读取eventfd */
+    initval = 4;
+    open_eventfd(initval, 0);
+    printf("init counter = %lu\n", initval);
 
-    //create thread
-    if(pthread_create(&tid1, NULL, threadFunc, NULL) < 0){
-        perror("pthread_create");
-    }
+    test(2);
 
-    while(i++ < 3){
-        buf += i;
-        rc = write(efd, &buf, sizeof(buf));
+    close_eventfd(efd);
 
-        if(rc != 8){
-            perror("write");
-        }
-        sleep(2);
-    }//end while
-    pthread_join(tid1, &status);
-    close(efd);
+    printf("change to SEMAPHORE way\n");
 
-    buf = 3;
-    flags = EFD_SEMAPHORE;
-    initval = 5;
-    i = 0;
+    /* 初始值为4， 信号量方式维护counter */
+    initval = 4;
+    open_eventfd(initval, EFD_SEMAPHORE);
+    printf("init counter = %lu\n", initval);
 
-    efd = eventfd(initval, flags);     // blocking阻塞等待, 信号量方式使用eventfd
-    if(efd == -1){
-        perror("eventfd");
-    }
+    test(2);
 
-    //create thread
-    if(pthread_create(&tid2, NULL, threadFunc, NULL) < 0){
-        perror("pthread_create");
-    }
+    close_eventfd(efd);
 
-    while(i++ < 3){
-        buf += i;
-        rc = write(efd, &buf, sizeof(buf));
+    printf("change to NONBLOCK way\n");
 
-        if(rc != 8){
-            perror("write");
-        }
-        sleep(2);
-    }//end while
-    close(efd);
+    /* 初始值为4， NONBLOCK方式读eventfd */
+    initval = 4;
+    open_eventfd(initval, EFD_NONBLOCK);
+    printf("init counter = %lu\n", initval);
+
+    test(2);
+
+    close_eventfd(efd);
 
     return 0;
 }
-
